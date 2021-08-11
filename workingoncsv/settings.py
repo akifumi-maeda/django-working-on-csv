e3115ad1a64fd9a334428a0ea0f01a4d1270faf7
Akifumi Maeda <akifumi_maeda@maede.co.jp>
2021-08-11 07:03:26 +0000

    fix secret key

--- workingoncsv/settings.py
+++ workingoncsv/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j1+07p6&#^&rhurdvc17+tu)i9p=)42t4rumk!)_d2sylrej0o'
+# SECRET_KEY = 'django-insecure-j1+07p6&#^&rhurdvc17+tu)i9p=)42t4rumk!)_d2sylrej0o'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-j1+07p6&#^&rhurdvc17+tu)i9p=)42t4rumk!)_d2sylrej0o')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
